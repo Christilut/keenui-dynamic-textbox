05c77a5d3fe438e5bf43eae21aeff798dd2b0471
Christiaan Maks <christilut@gmail.com>
2017-03-27 23:25:17 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+    <ui-textbox v-model="newContent['textbox']" placeholder="Textbox"></ui-textbox>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  },
+  data () {
+    return {
+      newContent: {}
+    }
+  },
+  created () {
+    this.newContent['textbox'] = 'test'
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../node_modules/keen-ui/dist/keen-ui.min';
+</style>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
